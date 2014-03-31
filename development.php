4352899a7f779f5decf57875fe7fc801cdd9782d
Shehroze Bhatti <playboyshehroze@yahoo.com>
2014-03-31 20:01:43 +0100

    Website for COMP2014

---
+++ development.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>COMP2014 EngduinoIDE</title>
+<link rel = "stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel = "stylesheet" type="text/css" href="bootstrap/css/main_style.css" />
+<link rel = "stylesheet" type="text/css" href="shadowbox-3.0.3/shadowbox.css" />
+
+<script src="http://use.edgefonts.net/lato.js"></script>
+
+<script src="shadowbox-3.0.3/shadowbox.js"></script>
+<script type="text/javascript">
+	Shadowbox.init();
+</script>
+
+</head>
+
+<body style = "background-color:#FFF ;">
+	<?php include("includes/header.php") ; ?>
+    
+    <div class = "main_container">
+    	<div class = "main_img_container">
+        	<a href = "graphics/Introduction.png"  rel="shadowbox" title="Analysis Class Diagram 1" ><img src="graphics/Introduction.png" class = "architectural_diagram"/></a>
+            
+            <a href = "graphics/Introduction.png"  rel="shadowbox" title="Analysis Class Diagram 2" ><img src="graphics/analysis_diagram_2.png" class = "architectural_diagram_2" />
+            
+            </a>
+        </div>   
+	</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>COMP2014 EngduinoIDE</title>
+<link rel = "stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel = "stylesheet" type="text/css" href="bootstrap/css/main_style.css" />
+<script src="http://use.edgefonts.net/lato.js"></script>
+
+
+</head>
+
+<body>
+	<?php include("includes/header.php") ; ?>
+    
+    <div class = "main_container">
+    	<div class = "main_img_container_index">
+        	<img src="graphics/engduino_bg.png" class = "main_engduino_bg" />
+        </div>    
+        
+        <div class = "product_feature_list">
+        	<p style = "font-size:20px;">Product Features</p>
+            <ul>
+        		<li>The engduino IDE has a drag and drop interface suitable for use in schools</li>
+                <li>The engduino IDE graphically represent different programming elements such as inputs, outputs and flow control</li>
+                <li>The engduino IDE is able to cross-compile the code to Atmel AVR machine code for the Engduino processor to run</li>
+                <li>The engduino IDE is able to upload the machine code on to an Engduino</li>
+                <li>The engduino IDE provides a view of the textual code for the use of students transitioning from graphical programming languages</li>
+                <li>The engduino IDE has line numbers in the code view to make debugging easier</li>
+                <li>The engduino IDE allow the use of the different sensors on the Engduino in the code</li>
+                
+                <li>The engduino IDE allows the manipulation of flow control when programming</li>
+                <li>The engduino IDE provides interactive help if the user has problem using the software</li>
+                <li>The engduino IDE allows users to save sketches/programs</li>
+                
+                <li>The engduino IDE allows the user to save sketches as modules which can then is used in other sketches</li>
+                <li>The engduino IDE is able to print out the code so that teachers can use it for marking</li>
+                <li>The engduino IDE is able to save a code sketch to a file</li>
+                <li>The engduino IDE is able to load previously saved sketches from a file</li>
+                <li>The engduino IDE is able to load multiple sketches at the same time</li>
+                
+        	</ul>
+        </div>
+	</div>
+
+</body>
+</html>
